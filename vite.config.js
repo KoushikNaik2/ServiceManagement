eb083585ef8a45c7bd1a989a4e00a42f025a6b52
Koushik Naik <koushiknaik@example.com>
2026-06-25 13:40:53 +0530

    fix: commit dist build, fix vite base path, fix vercel asset routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -27,7 +28,5 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    base: '/',
   },
-  base: '/',
 });
